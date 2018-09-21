8e01f5df0cbe62924d73fffdffd3744981f8caea
satoshi <consultation.pharmcist2@gmail.com>
2018-09-22 00:06:30 +0900

    config/routes.rbのuser routeの修正、user_viewの各ファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'prices/insert'
 
-  resources :user, only: [:new, :create, :show, :edit, :update] do
+  resources :users, only: [:new, :create, :show, :edit, :update] do
     collection do
       post  :confirm
     end
