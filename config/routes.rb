3427185c2728d37090ab97671e7c3976f28446bc
yoshikiosuga <consultation.pharmacist@gmail.com>
2018-10-07 18:28:34 +0900

    prscription を scaffold した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :prescriptions do
+    collection do
+      post  :confirm
+    end
+  end
+  
   resources :users, only: [:new, :create, :show, :edit, :update] do
     collection do
       post  :confirm
