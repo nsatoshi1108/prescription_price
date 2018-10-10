9f763847ce1ffa815ebe58b584e42e51843104f3
yoshikiosuga <consultation.pharmacist@gmail.com>
2018-10-11 01:23:38 +0900

    フォームの飲み方タグにauto complete機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
     collection do
       post  :confirm
       get :autocomplete_medicine_name
+      get :autocomplete_dose_method_take_point
     end
   end
-  
+
   resources :users, only: [:new, :create, :show, :edit, :update] do
     collection do
       post  :confirm
