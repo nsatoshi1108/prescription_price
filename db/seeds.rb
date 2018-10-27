3303d1b4e7d4c958c55f974e38bdf9563c618ad2
yoshikiosuga <consultation.pharmacist@gmail.com>
2018-10-28 02:08:56 +0900

    addition_sumに初期状態で数値を追加できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,41 @@ require "csv"
 CSV.foreach('db/pharmacy_tokyo.csv') do |row|
 
   pharmacy = Pharmacy.create(pharmacy_code: row[0],name: row[1],postal: row[2],address: row[4],tel: row[5])
+  addition_score = 0
 
   if row[3].present?
     if row[3].match(/調基１/)
       pharmacy.pharmacy_additions.create(addition_id: 1)
+      addition_score += 41
     elsif row[3].match(/調基２/)
       pharmacy.pharmacy_additions.create(addition_id: 2)
+      addition_score += 25
     elsif row[3].match(/調基３イ/)
       pharmacy.pharmacy_additions.create(addition_id: 3)
+      addition_score += 20
     elsif row[3].match(/調基３ロ/)
       pharmacy.pharmacy_additions.create(addition_id: 4)
+      addition_score += 15
     elsif row[3].match(/調基特１/)
       pharmacy.pharmacy_additions.create(addition_id: 5)
+      addition_score += 10
     end
 
     if row[3].match(/地支体/)
       pharmacy.pharmacy_additions.create(addition_id: 6)
+      addition_score += 35
     end
 
     if row[3].match(/後発調１/)
       pharmacy.pharmacy_additions.create(addition_id: 7)
+      addition_score += 18
     elsif row[3].match(/後発調２/)
       pharmacy.pharmacy_additions.create(addition_id: 8)
+      addition_score += 22
     elsif row[3].match(/後発調３/)
       pharmacy.pharmacy_additions.create(addition_id: 9)
+      addition_score += 26
     end
+    pharmacy.update(addition_sum: addition_score)
   end
-end
-
-
-CSV.foreach('db/internal_H30_8_29.csv') do |row|
-
-  Medicine.create(original_price: row[12],generic_price: row[13],classification: row[5],usage: row[0], code: row[1].to_i, name: row[7], standard: row[3])
-
 end
