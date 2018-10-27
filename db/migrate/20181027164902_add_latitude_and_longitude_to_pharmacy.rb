4315d0d7c412650dc7de9a04cb929e4b46bb2375
yoshikiosuga <consultation.pharmacist@gmail.com>
2018-10-28 02:08:03 +0900

    緯度経度のカラムに :precision => 9, :scale => 6 を設定
    整数３桁、小数点６桁の意味（google mapの最大拡大時の必要桁数）

--- db/migrate/20181027164902_add_latitude_and_longitude_to_pharmacy.rb
+++ db/migrate/20181027164902_add_latitude_and_longitude_to_pharmacy.rb
@@ -1,6 +1,6 @@
 class AddLatitudeAndLongitudeToPharmacy < ActiveRecord::Migration[5.1]
   def change
-    add_column :pharmacies, :latitude, :decimal, :null => false
-    add_column :pharmacies, :longitude, :decimal, :null => false
+    add_column :pharmacies, :latitude, :decimal,, :precision => 9, :scale => 6, :null => false
+    add_column :pharmacies, :longitude, :decimal, :precision => 9, :scale => 6, :null => false
   end
 end
